1d56e1c77593d71eec589ab2a809f5462c0851da
AuroraLantean <raymondlieu.crypto@runbox.com>
2025-12-19 13:51:35 +0200

    add thisError code; change indentation in updateConfig

--- src/instructions/updateConfig.rs
+++ src/instructions/updateConfig.rs
@@ -4,103 +4,103 @@ use pinocchio_log::log;
 
 use crate::{check_pda, instructions::check_signer, parse_u64, writable};
 pub struct UpdateConfigStatus<'a> {
-    pub authority: &'a AccountInfo,
+  pub authority: &'a AccountInfo,
 }
 pub struct UpdateConfigFee<'a> {
-    pub authority: &'a AccountInfo,
+  pub authority: &'a AccountInfo,
 }
 pub struct UpdateConfigAuthority<'a> {
-    pub authority: &'a AccountInfo,
+  pub authority: &'a AccountInfo,
 }
 
 /// Update Config PDA
 pub struct UpdateConfig<'a> {
-    pub authority: &'a AccountInfo,
-    pub pda1: &'a AccountInfo,
-    pub pda2: &'a AccountInfo,
-    pub datalen: usize,
-    pub num8a: u8,
-    pub num8b: u8,
-    pub num64a: u64,
-    pub num64b: u64,
+  pub authority: &'a AccountInfo,
+  pub pda1: &'a AccountInfo,
+  pub pda2: &'a AccountInfo,
+  pub datalen: usize,
+  pub num8a: u8,
+  pub num8b: u8,
+  pub num64a: u64,
+  pub num64b: u64,
 }
 impl<'a> UpdateConfig<'a> {
-    pub const DISCRIMINATOR: &'a u8 = &13;
+  pub const DISCRIMINATOR: &'a u8 = &13;
 
-    pub fn process(self) -> ProgramResult {
-        let UpdateConfig {
-            authority,
-            pda1,
-            pda2,
-            datalen: u32,
-            num8a,
-            num8b,
-            num64a,
-            num64b,
-        } = self;
-        log!("UpdateConfig process()");
-        check_signer(authority)?;
-        writable(pda1)?;
-        //writable(pda2)?;
+  pub fn process(self) -> ProgramResult {
+    let UpdateConfig {
+      authority,
+      pda1,
+      pda2,
+      datalen: _,
+      num8a: _,
+      num8b: _,
+      num64a: _,
+      num64b: _,
+    } = self;
+    log!("UpdateConfig process()");
+    check_signer(authority)?;
+    writable(pda1)?;
+    //writable(pda2)?;
 
-        log!("UpdateConfig 2");
-        check_pda(pda1)?;
-        check_pda(pda2)?;
-        log!("UpdateConfig 3");
+    log!("UpdateConfig 2");
+    check_pda(pda1)?;
+    check_pda(pda2)?;
+    log!("UpdateConfig 3");
 
-        log!("UpdateConfig 4");
+    log!("UpdateConfig 4");
 
-        log!("UpdateConfig 5");
-        match self.datalen as usize {
-            len if len == size_of::<UpdateConfigStatus>() => self.update_status()?,
-            len if len == size_of::<UpdateConfigFee>() => self.update_fee()?,
-            len if len == size_of::<UpdateConfigAuthority>() => self.update_authority()?,
-            _ => return Err(ProgramError::InvalidInstructionData),
-        }
-        Ok(())
+    log!("UpdateConfig 5");
+    match self.datalen as usize {
+      len if len == size_of::<UpdateConfigStatus>() => self.update_status()?,
+      len if len == size_of::<UpdateConfigFee>() => self.update_fee()?,
+      len if len == size_of::<UpdateConfigAuthority>() => self.update_authority()?,
+      _ => return Err(ProgramError::InvalidInstructionData),
     }
-    pub fn update_status(self) -> ProgramResult {
-        Ok(())
-    }
-    pub fn update_fee(self) -> ProgramResult {
-        Ok(())
-    }
-    pub fn update_authority(self) -> ProgramResult {
-        Ok(())
-    }
+    Ok(())
+  }
+  pub fn update_status(self) -> ProgramResult {
+    Ok(())
+  }
+  pub fn update_fee(self) -> ProgramResult {
+    Ok(())
+  }
+  pub fn update_authority(self) -> ProgramResult {
+    Ok(())
+  }
 }
 impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for UpdateConfig<'a> {
-    type Error = ProgramError;
+  type Error = ProgramError;
 
-    fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
-        log!("UpdateConfig try_from");
-        let (data, accounts) = value;
-        log!("accounts len: {}, data len: {}", accounts.len(), data.len());
-        let datalen = data.len();
+  fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
+    log!("UpdateConfig try_from");
+    let (data, accounts) = value;
+    log!("accounts len: {}, data len: {}", accounts.len(), data.len());
+    let datalen = data.len();
 
-        let [authority, pda1, pda2] = accounts else {
-            return Err(ProgramError::NotEnoughAccountKeys);
-        };
-        let num8a = data[0];
-        let num8b = data[1];
-        let num64a = parse_u64(&data[2..10])?;
-        let num64b = parse_u64(&data[11..19])?;
-        log!(
-            "num8a: {}, num8b: {}, num64a: {}, num64b: {}",
-            num8a,
-            num8b,
-            num64a,
-            num64b
-        );
-        Ok(Self {
-            authority,
-            pda1,
-            pda2,
-            datalen,
-            num8a,
-            num8b,
-            num64a,
-            num64b,
-        })
-    }
+    let [authority, pda1, pda2] = accounts else {
+      return Err(ProgramError::NotEnoughAccountKeys);
+    };
+    let num8a = data[0];
+    let num8b = data[1];
+    let num64a = parse_u64(&data[2..10])?;
+    let num64b = parse_u64(&data[11..19])?;
+    log!(
+      "num8a: {}, num8b: {}, num64a: {}, num64b: {}",
+      num8a,
+      num8b,
+      num64a,
+      num64b
+    );
+    Ok(Self {
+      authority,
+      pda1,
+      pda2,
+      datalen,
+      num8a,
+      num8b,
+      num64a,
+      num64b,
+    })
+  }
 }
--- src/instructions/utils.rs
+++ src/instructions/utils.rs
@@ -1,62 +1,95 @@
-use pinocchio::{account_info::AccountInfo, program_error::ProgramError};
+//use num_derive::FromPrimitive;
+use pinocchio::{
+  account_info::AccountInfo,
+  program_error::{ProgramError, ToStr},
+};
+use thiserror::Error;
+
+#[derive(Clone, Debug, Eq, Error, PartialEq)] //FromPrimitive
+pub enum CustomError {
+  #[error("NotRentExempt")]
+  NotRentExempt,
+}
+impl From<CustomError> for ProgramError {
+  fn from(e: CustomError) -> Self {
+    ProgramError::Custom(e as u32)
+  }
+}
+
+//Deserialize Errors from Raw Values
+impl TryFrom<u32> for CustomError {
+  type Error = ProgramError;
+  fn try_from(error: u32) -> Result<Self, Self::Error> {
+    match error {
+      0 => Ok(CustomError::NotRentExempt),
+      _ => Err(ProgramError::InvalidArgument),
+    }
+  }
+}
+//Human Readable Errors
+impl ToStr for CustomError {
+  fn to_str<E>(&self) -> &'static str {
+    match self {
+      CustomError::NotRentExempt => "rent-exempt",
+    }
+  }
+}
 
 const TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET: usize = 165;
 pub const TOKEN_2022_MINT_DISCRIMINATOR: u8 = 0x01;
 pub const TOKEN_2022_TOKEN_ACCOUNT_DISCRIMINATOR: u8 = 0x02;
 
 pub fn check_mint_interface(mint: &AccountInfo) -> Result<(), ProgramError> {
-    if !mint.is_owned_by(&pinocchio_token_2022::ID) {
-        //legacy token
-        if !mint.is_owned_by(&pinocchio_token::ID) {
-            return Err(ProgramError::Custom(440));
-        } else {
-            if mint.data_len().ne(&pinocchio_token::state::Mint::LEN) {
-                return Err(ProgramError::Custom(441));
-            }
-        }
+  if !mint.is_owned_by(&pinocchio_token_2022::ID) {
+    //legacy token
+    if !mint.is_owned_by(&pinocchio_token::ID) {
+      return Err(ProgramError::Custom(440));
     } else {
-        //Token2022
-        let data = mint.try_borrow_data()?;
-
-        if data.len().ne(&pinocchio_token::state::Mint::LEN) {
-            if data.len().le(&TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET) {
-                return Err(ProgramError::Custom(442));
-            }
-            if data[TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET].ne(&TOKEN_2022_MINT_DISCRIMINATOR) {
-                return Err(ProgramError::Custom(443));
-            }
-        }
+      if mint.data_len().ne(&pinocchio_token::state::Mint::LEN) {
+        return Err(ProgramError::Custom(441));
+      }
     }
-    Ok(())
+  } else {
+    //Token2022
+    let data = mint.try_borrow_data()?;
+
+    if data.len().ne(&pinocchio_token::state::Mint::LEN) {
+      if data.len().le(&TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET) {
+        return Err(ProgramError::Custom(442));
+      }
+      if data[TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET].ne(&TOKEN_2022_MINT_DISCRIMINATOR) {
+        return Err(ProgramError::Custom(443));
+      }
+    }
+  }
+  Ok(())
 }
 
 pub fn check_tokacct_interface(ata: &AccountInfo) -> Result<(), ProgramError> {
-    if !ata.is_owned_by(&pinocchio_token_2022::ID) {
-        //Legacy ATA
-        if !ata.is_owned_by(&pinocchio_token::ID) {
-            return Err(ProgramError::Custom(444));
-        } else {
-            if ata
-                .data_len()
-                .ne(&pinocchio_token::state::TokenAccount::LEN)
-            {
-                return Err(ProgramError::Custom(445));
-            }
-        }
+  if !ata.is_owned_by(&pinocchio_token_2022::ID) {
+    //Legacy ATA
+    if !ata.is_owned_by(&pinocchio_token::ID) {
+      return Err(ProgramError::Custom(444));
     } else {
-        //Token2022 ATA
-        let data = ata.try_borrow_data()?;
-
-        if data.len().ne(&pinocchio_token::state::TokenAccount::LEN) {
-            if data.len().le(&TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET) {
-                return Err(ProgramError::Custom(446));
-            }
-            if data[TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET]
-                .ne(&TOKEN_2022_TOKEN_ACCOUNT_DISCRIMINATOR)
-            {
-                return Err(ProgramError::Custom(447));
-            }
-        }
+      if ata
+        .data_len()
+        .ne(&pinocchio_token::state::TokenAccount::LEN)
+      {
+        return Err(ProgramError::Custom(445));
+      }
     }
-    Ok(())
+  } else {
+    //Token2022 ATA
+    let data = ata.try_borrow_data()?;
+
+    if data.len().ne(&pinocchio_token::state::TokenAccount::LEN) {
+      if data.len().le(&TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET) {
+        return Err(ProgramError::Custom(446));
+      }
+      if data[TOKEN_2022_ACCOUNT_DISCRIMINATOR_OFFSET].ne(&TOKEN_2022_TOKEN_ACCOUNT_DISCRIMINATOR) {
+        return Err(ProgramError::Custom(447));
+      }
+    }
+  }
+  Ok(())
 }
