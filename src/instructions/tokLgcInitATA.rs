505daa07202eb87c1250c976e9e31af3c4f4013f
AuroraLantean <raymondlieu.crypto@runbox.com>
2025-12-20 12:19:28 +0200

    fix tokLgcInitATA indentation

--- src/instructions/tokLgcInitATA.rs
+++ src/instructions/tokLgcInitATA.rs
@@ -1,6 +1,6 @@
 use crate::{
-    check_mint0a, empty_data, empty_lamport, executable, instructions::check_signer, rent_exempt,
-    writable,
+  check_mint0a, empty_data, empty_lamport, executable, instructions::check_signer, rent_exempt,
+  writable,
 };
 use core::convert::TryFrom;
 use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult};
@@ -8,84 +8,84 @@ use pinocchio_log::log;
 
 /// Token Legacy Init ATA(Associated Token Account)
 pub struct TokenLgcInitAta<'a> {
-    pub payer: &'a AccountInfo,
-    pub to_wallet: &'a AccountInfo,
-    pub mint: &'a AccountInfo,
-    pub token_account: &'a AccountInfo,
-    pub token_program: &'a AccountInfo,
-    pub system_program: &'a AccountInfo,
-    pub atoken_program: &'a AccountInfo,
+  pub payer: &'a AccountInfo,
+  pub to_wallet: &'a AccountInfo,
+  pub mint: &'a AccountInfo,
+  pub token_account: &'a AccountInfo,
+  pub token_program: &'a AccountInfo,
+  pub system_program: &'a AccountInfo,
+  pub atoken_program: &'a AccountInfo,
 }
 impl<'a> TokenLgcInitAta<'a> {
-    pub const DISCRIMINATOR: &'a u8 = &3;
+  pub const DISCRIMINATOR: &'a u8 = &3;
 
-    pub fn process(self) -> ProgramResult {
-        let TokenLgcInitAta {
-            payer, //signer
-            to_wallet,
-            mint,
-            token_account,
-            token_program,
-            system_program,
-            atoken_program: _,
-        } = self;
-        log!("TokenLgcInitAta process()");
-        check_signer(payer)?;
-        executable(token_program)?;
+  pub fn process(self) -> ProgramResult {
+    let TokenLgcInitAta {
+      payer, //signer
+      to_wallet,
+      mint,
+      token_account,
+      token_program,
+      system_program,
+      atoken_program: _,
+    } = self;
+    log!("TokenLgcInitAta process()");
+    check_signer(payer)?;
+    executable(token_program)?;
 
-        log!("TokenLgcInitAta 1");
-        rent_exempt(mint, 0)?;
-        check_mint0a(mint, token_program)?;
-        //writable(mint)?;//Shank IDL definition
+    log!("TokenLgcInitAta 1");
+    rent_exempt(mint, 0)?;
+    check_mint0a(mint, token_program)?;
+    //writable(mint)?;//Shank IDL definition
 
-        log!("TokenLgcInitAta 2");
-        empty_lamport(token_account)?;
-        empty_data(token_account)?;
-        writable(token_account)?;
+    log!("TokenLgcInitAta 2");
+    empty_lamport(token_account)?;
+    empty_data(token_account)?;
+    writable(token_account)?;
 
-        log!("Make ATA Token Account");
-        pinocchio_associated_token_account::instructions::Create {
-            funding_account: payer, // Keypair
-            account: token_account,
-            wallet: to_wallet,
-            mint: mint,
-            system_program: system_program,
-            token_program: token_program,
-        }
-        .invoke()?;
-        Ok(())
+    log!("Make ATA Token Account");
+    pinocchio_associated_token_account::instructions::Create {
+      funding_account: payer, // Keypair
+      account: token_account,
+      wallet: to_wallet,
+      mint: mint,
+      system_program: system_program,
+      token_program: token_program,
     }
-    pub fn init_if_needed(self) -> ProgramResult {
-        match empty_lamport(self.token_account) {
-            Ok(_) => Self::process(self),
-            Err(_) => Ok(()),
-        }
+    .invoke()?;
+    Ok(())
+  }
+  pub fn init_if_needed(self) -> ProgramResult {
+    match empty_lamport(self.token_account) {
+      Ok(_) => Self::process(self),
+      Err(_) => Ok(()),
     }
+  }
 }
 impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for TokenLgcInitAta<'a> {
-    type Error = ProgramError;
+  type Error = ProgramError;
 
-    fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
-        log!("TokenLgcInitAta try_from");
-        let (data, accounts) = value;
-        log!("accounts len: {}, data len: {}", accounts.len(), data.len());
+  fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
+    log!("TokenLgcInitAta try_from");
+    let (data, accounts) = value;
+    log!("accounts len: {}, data len: {}", accounts.len(), data.len());
 
-        let [payer, to_wallet, mint, token_account, token_program, system_program, atoken_program] =
-            accounts
-        else {
-            return Err(ProgramError::NotEnoughAccountKeys);
-        };
-        log!("TokenLgcInitAta try_from end");
-        Ok(Self {
-            payer,
-            to_wallet,
-            mint,
-            token_account,
-            token_program,
-            system_program,
-            atoken_program,
-        })
-    }
+    let [payer, to_wallet, mint, token_account, token_program, system_program, atoken_program] =
+      accounts
+    else {
+      return Err(ProgramError::NotEnoughAccountKeys);
+    };
+    log!("TokenLgcInitAta try_from end");
+    Ok(Self {
+      payer,
+      to_wallet,
+      mint,
+      token_account,
+      token_program,
+      system_program,
+      atoken_program,
+    })
+  }
 }
 /*find token_account from to_wallet & token mint ... but ignored because this make the program bigger
 cargo add spl-associated-token-account
